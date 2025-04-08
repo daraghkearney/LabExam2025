1c13ee52917eda377ce75f933480b73cc1772b46
DARAGHKEARNEY-STUDEN <g00438215@atu.ie>
2025-04-08 09:52:32 +0100

    Test added for Getters

--- src/test/java/PassengerTest.java
+++ src/test/java/PassengerTest.java
@@ -1,4 +1,14 @@
+import ie.atu.exam2025.Passenger;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertations.*;
 public class PassengerTest {
+    @Test
+    public void testGetters() {
+        Passenger passenger = new Passenger("Mr", "Alex", "Murphy", 30, "123456");
+
+        assertEquals("Mr", passenger.getTitle());
+        assertEquals("Alex", passenger.getFirstName());
+        assertEquals("Murphy", passenger.getLastName());
+        assertEquals(30, passenger.getAge());
+    }
 }
